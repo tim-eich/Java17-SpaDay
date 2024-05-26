25734049b12f1e3618731635c19a37f41201ed52
Tim-E <caffeinated.ink@gmail.com>
2024-05-26 12:10:23 -0400

    spa day user sign up, first commit

--- src/main/java/org/launchcode/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/controllers/SpaDayController.java
@@ -1,6 +1,7 @@
 package org.launchcode.controllers;
 
 import org.launchcode.models.Client;
+import org.launchcode.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,14 @@
+package org.launchcode.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @GetMapping("/user/add")
+    public String displayAddUserForm() {
+        return "/user/add.html";
+    }
+}
