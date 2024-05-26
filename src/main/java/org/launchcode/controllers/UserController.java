7e4fa2358faa5a328351be7d45c73e6af556a073
Tim-E <caffeinated.ink@gmail.com>
2024-05-26 17:47:22 -0400

    user/index not working

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,14 +1,32 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
-    @GetMapping("/user/add")
+
+    @GetMapping("/add")
     public String displayAddUserForm() {
-        return "/user/add.html";
+        return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
+        }
+        else {
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
+
     }
 }
